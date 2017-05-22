9a6d5dc729584c0de8aa8afe78f30d4fad2e74c6
hnliyuan <258620825@qq.com>
2017-05-22 17:32:43 +0800

    添加了一些功能

--- JSTRAINNING/resp/src/index/search/Search.js
+++ JSTRAINNING/resp/src/index/search/Search.js
@@ -1,209 +1,175 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, ScrollView, TouchableHighlight, Text, StatusBar, WebView,TextInput } from 'react-native'
+import {View, ScrollView, StyleSheet,TextInput,Text,Alert  } from 'react-native'
 import Util from '../../util/Util'
 import Icon from 'react-native-vector-icons/MaterialIcons'
-import {
-    MapView, 
-    MapTypes, 
-    Geolocation 
-} from 'react-native-baidu-map';
-import ModalPicker from 'react-native-modal-picker'
-
-import Menu, { MenuContext, MenuOptions, MenuOption, MenuTrigger } from 'react-native-menu';
-import {
-  List,
-  ListItem,
-  SideMenu
-} from 'react-native-elements'
-
 
 
 class Search extends Component{
-	
     constructor (props) {
 	    super(props)
-        this.state = {
-            mayType: MapTypes.NORMAL,
-            zoom: 15, 
-            trafficEnabled: false,
-            baiduHeatMapEnabled: false,
-            textInputValue:''
-        };
+		this.state = {
+			memoryList :[],
+	    	pumpList :[],
+			text:'',
+		}
   	}
-    
-	componentDidMount() { // 获取位置
-        Geolocation.getCurrentPosition().then(
-            (data) => {
-                this.setState({
-                    zoom:18,
-                    markers:[{
-                        latitude:data.latitude,
-                        longitude:data.longitude,
-                        title:'我的位置'
-                    }],
-                    center:{
-                        latitude:data.latitude,
-                        longitude:data.longitude,
-                    }
-                })
-            }
-        ).catch(error => {
-            console.warn(error,'error')
-        })
-    }
-    
-	render() {
-		let index = 0;
-        const data = [
-            { key: index++, section: true, label: '湖南' },
-            { key: index++, label: '长沙' },
-            { key: index++, label: '湘潭' },
-            { key: index++, label: '株洲' },
-            { key: index++, label: '浏阳' },
-            { key: index++, label: '益阳' },
-            { key: index++, section: true, label: '广东' },
-            { key: index++, label: '广州' },
-            { key: index++, label: '佛山' },
-            { key: index++, label: '东莞' },
-            { key: index++, label: '珠海' },
-            { key: index++, label: '韶关' },
-            { key: index++, label: '清远' },
-            { key: index++, label: '梅州' },
-            { key: index++, label: '惠州' }
-        ];
+	componentWillMount(){
+		this.searchPumpByName();
+	}
 
-		const { navigate } = this.props.navigation
-		const { toggleSideMenu } = this.props
-		const { mapShow } = this.state
+  	searchPumpByName = (text) => {
+
+		if(text){
+			let filterList = [];
+            for(var i = 0 ; i < this.state.memoryList.length; i++){
+            	item = this.state.memoryList[i];
+                if(item.sbms.includes(text.text)|| item.dqms.includes(text.text)|| item.dwms.includes(text.text)|| item.htms.includes(text.text)){
+                    filterList.push(item);
+                }
+			}
+            this.setState({text:text.text,pumpList:filterList});
+		}else{
+			if(this.state.memoryList.length>1){
+				this.setState({pumpList:this.state.memoryList})
+			}
+            fetch('http://192.168.48.99:8088/reactNativeApp/Search!getAllPumps.action').then((response) =>{
+                if(200 === response.status){
+                	//地区、客户、项目、水泵名称
+                	data = JSON.parse(response._bodyInit);
+					if(data){
+						this.setState({pumpList:data.data,memoryList:data.data});
+					}
+            	}
+			}).catch((error) => {
+					Alert.alert(
+					'请求出错',
+					error,
+				)
+			})
+		}
+	}
+
+
+	render() {
+    	const { toggleSideMenu } = this.props;
+		const { navigate } = this.props.navigation;
+		if(this.state.pumpList.length <= 0){
+			return (<View style={searchStyles.searchResult}><Text style={{width:Util.size.width,textAlign:'center',marginTop:15,fontSize:16,color:'#08527a'}}>正在查询请稍后</Text></View>)
+		}else{
+
+
 		return (
-			<MenuContext style={{ flex: 1 }}>
-				<View style={styles.container}>
-					<View style={styles.header}>
-						<View style={styles.leftBackView}>
-			        		<Icon
-							  name='account-circle'
-							  color='white'
-							  size={32}
-							  onPress={() => toggleSideMenu()} />
-						</View>
-						<View style={styles.centerTitle}>
-			                <ModalPicker
-			                    data={data}
-			                    initValue="选择地区..."
-			                    onChange={(option)=>{ this.setState({textInputValue:option.label})}}
-		                    	>
-								<TextInput
-									style={styles.centerText}
-									editable={false}
-									placeholder = "选择地区..."
-									placeholderTextColor = 'white'
-									value={this.state.textInputValue} />
-			                </ModalPicker>
-						</View>
-						<View style={styles.rightBackView}>
-							
-								<Menu onSelect={(value) => alert(`User selected the number ${value}`)}>
-									<MenuTrigger>
-										<Icon
-										  name='add-circle'
-										  color='white'
-										  size={32}
-										  />
-								    </MenuTrigger>
-								    <MenuOptions optionsContainerStyle={{ marginTop:45 ,borderRadius: 10,backgroundColor:'#0b77b2',width:120}}>
-								        <MenuOption value={1} style={{borderBottomWidth:0.5,borderBottomColor:'white'}}>
-								        	<View style={{flexDirection: 'row'}}>
-								        		 <Icon
-												  name='search'
-												  color='white'
-												  size={20}
-												  />
-								          		<Text style={{fontSize:14,color:'white',textAlign:'center',marginLeft:10}}>查询水泵</Text>
-								        	</View>
-								         
-								        </MenuOption>
-								        <MenuOption value={2} style={{borderBottomWidth:0.5,borderBottomColor:'white'}} >
-												<View style={{flexDirection: 'row'}}>
-								        		 <Icon
-												  name='insert-drive-file'
-												  color='white'
-												  size={20}
-												  />
-								          		<Text style={{fontSize:14,color:'white',textAlign:'center',marginLeft:10}}>查看报表</Text>
-								        	</View>
-								        </MenuOption>
-								        <MenuOption value={3} style={{}} >
-												<View style={{flexDirection: 'row'}}>
-								        		 <Icon
-												  name='wallpaper'
-												  color='white'
-												  size={20}
-												  />
-								          		<Text style={{fontSize:14,color:'white',textAlign:'center',marginLeft:10}}>扫一扫</Text>
-								        	</View>
-								        </MenuOption>
-							      	</MenuOptions>
-								</Menu>
-							
-						</View>
-					</View>
-						<View style={styles.center}>
-							
-						</View>
+			<View style={searchStyles.container}>
+				<View style={searchStyles.searchContent}>
+					<Icon
+						name='account-circle'
+						color='white'
+						size={32}
+						style={{marginLeft:10,width:40}}
+						onPress={() => toggleSideMenu()} />
+					<TextInput
+						style={{width:Util.size.width - 100,color:'white'}}
+						onChangeText={(text) => this.searchPumpByName({text})}
+						placeholder = '请输入...'
+						placeholderTextColor = 'white'
+    					underlineColorAndroid="white"
+    					selectionColor="white"
+            			value={this.state.text}
+						/>
+					<Icon
+						name='close'
+						color='white'
+						size={32}
+						onPress={() => {
+							if(this.state.memoryList.length>0){
+                                this.setState({text:'',pumpList:this.state.memoryList})
+							}else{
+                                this.setState({text:''})
+							}
+
+					}} />
 				</View>
-			</MenuContext>
+				<View style={searchStyles.container}>
+						<ScrollView
+							contentContainerStyle={{flexDirection: 'column',marginLeft:10}}
+							/* 不显示垂直方向的滚动条 */
+							showsVerticalScrollIndicator={false}
+							>
+						{
+							this.state.pumpList.map((item, i) => (
+								<View style={searchStyles.pumpView} key={i}>
+									<View style={{flex:2,alignItems:'center',justifyContent:'center'}}>
+										<Icon
+											name='av-timer'
+											color='gray'
+											size={60}
+												/>
+									</View>
+									<View style={{flex:8,flexDirection:'column',justifyContent:'center',marginLeft:10}}>
+										<View style={{flexDirection:'row'}}>
+											<Text style={{fontSize:16,color:'black'}}>{item.sbms}</Text>
+										</View>
+										<View style={{flexDirection:'row'}}>
+											<Text style={{fontSize:12}}>地区 : {item.dqms}</Text>
+										</View>
+										<View style={{flexDirection:'row'}}>
+											<Text style={{fontSize:12}}>客户 : {item.dwms}</Text>
+										</View>
+										<View style={{flexDirection:'row'}}>
+											<Text style={{fontSize:12}}>合同 : {item.htms}</Text>
+										</View>
+									</View>
+									<View style={{flex:2,alignItems:'center',justifyContent:'center'}}>
+										<Icon
+											name='last-page'
+											color='gray'
+											size={36}
+											onPress={()=>{
+												let pump = {title:item.sbms+'@'+item.id};
+												navigate('Pump',{pump:pump});
+											}}
+											/>
+									</View>
+								</View>
+							))
+						}
+						</ScrollView>
+				</View>
+
+			</View>
 		)
 	}
-	
-	
+	}
 }
 
-styles = StyleSheet.create({
+const searchStyles = StyleSheet.create({
 	container:{
-		flex: 1,
-		flexDirection: 'column',
+		flex:1,
+        backgroundColor:'white',
+        flexDirection: 'column',
+	},
+	searchContent:{
+		height:50,
+		flexDirection: 'row',
+        alignItems: 'center',
+		backgroundColor:'#08527a'
+	},
+	scrollContainer:{
+        backgroundColor:'black'
+    },
+	searchResult:{
+		flex:1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor:'white',
 	},
-    header:{
-        height: 50,
-        width: Util.size.width,
+	pumpView:{
         flexDirection: 'row',
-        backgroundColor: '#08527a'
-    },
-    center:{
-        height: Util.size.height-100-StatusBar.currentHeight,
-        width: Util.size.width,
-        flexDirection: 'row',
-    },
-    leftBackView:{
-    	height:50,
-        width: Util.size.width/3,
-	    alignItems: 'flex-start',
-	    paddingTop:9,
-	    paddingLeft:9,
-    },
-    rightBackView:{
-    	height:50,
-        width: Util.size.width/3,
-	    alignItems: 'flex-end',
-	    paddingTop:9,
-	    paddingRight:9,
-    },
-    centerTitle:{
-	    height: 50,
-        width: Util.size.width/3,
-    },
-    centerText:{
-	    fontSize: 20,
-	    color: 'white',
-	    textAlign:'center',
-	    paddingTop:10,
-    },
-    map: {
-    	height:Util.size.height-100-StatusBar.currentHeight,
-    	width:Util.size.width,
+        borderTopColor: 'gray',
+		borderTopWidth: 1,
+		margin:5
     }
-
 })
 
 export default Search
